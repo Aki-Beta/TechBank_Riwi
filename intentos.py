ec022787bc3c9bf678cde31927ab627d18dff0b3
julianvanegs <julivanegascri@gmail.com>
2026-03-02 16:02:52 -0500

    Update intentos.py

---
+++ intentos.py
@@ -0,0 +1,13 @@
+intentos = 3 
+while intentos> 0:
+    usuario = input("introduce el usuario ")
+    password = input("introduce la contraseña ")
+
+    if usuario == "tyler" and password == "tyler123":
+        print("has entrado al sistema")
+        break
+    else:
+        intentos -=1
+        print (f"incorrecto. te queda {intentos} intentos. \n")
+    if intentos == 0:
+        print ("cuenta bloqueada")
