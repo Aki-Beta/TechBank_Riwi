488c6f679c3cfd0a32ed36e07bf3c0d7140b052a
julianvanegs <julivanegascri@gmail.com>
2026-03-03 14:37:08 -0500

    Refactor saldo operation into consultar_saldo function

--- gestion_de_Saldo.py
+++ gestion_de_Saldo.py
@@ -1,3 +1,4 @@
-  elif operacion == "1":
-        historial.append(f"Saldo: +{saldo}")
-        print(f"Tu saldo actual es: {saldo}")
+def consultar_saldo(saldo, historial):
+    historial.append(f"Saldo: +{saldo}")
+    print(f"Tu saldo actual es: {saldo}")
+    return saldo
