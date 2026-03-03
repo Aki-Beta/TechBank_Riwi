084d2fa9329f8a2e2e6338bd58594cf12fe35f8c
Daniel Arci <danielarciniegaspua@gmail.com>
2026-03-03 14:10:37 -0500

    Merge de funcion de retiro

--- cajero_retiro.py
+++ cajero_retiro.py
@@ -1,7 +1,10 @@
+from validacion_numeros import pedirNumero
+saldo = 1000
+opcion = input()
 if opcion == "2":
-    retiro = float(input("Ingrese el monto a retirar: "))
+    retiro = pedirNumero()
     if 0 < retiro <= saldo:
         saldo -= retiro
-        print("Retiro exitoso. Nuevo saldo: ${saldo}")
+        print(f"Retiro exitoso. Nuevo saldo: {saldo}")
     else:
         print("Fondos insuficientes o monto no válido.")
