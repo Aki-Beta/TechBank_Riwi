6126757ee986b42b825a71c01542df106645b60f
johan <joelhernava@gmail.com>
2026-03-03 12:56:27 -0500

    nuevo

---
+++ configuracion.py
@@ -0,0 +1,7 @@
+print("Bienvenidos al Banco ")
+saldo=1000
+consultas= int(input("cuantas operaciones desea realizar")) 
+for i in range(consultas):
+    menu ()
+    consultas=("cuantas operaciones realizar")
+    
