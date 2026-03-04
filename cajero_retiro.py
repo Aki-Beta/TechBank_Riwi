67d1bbbf0f51ffe926cd28dbe8862d083e47f27c
Andrea2112Bernal <130117270+Andrea2112Jotanicia@users.noreply.github.com>
2026-03-04 12:37:20 -0500

    Commit funcion_retiro (04/03/2026)
    Se adaptó codigo de funcion_retiro en preparación para el merge

--- cajero_retiro.py
+++ cajero_retiro.py
@@ -1,7 +1,6 @@
-if opcion == "2":
-    retiro = float(input("Ingrese el monto a retirar: "))
-    if 0 < retiro <= saldo:
-        saldo -= retiro
-        print("Retiro exitoso. Nuevo saldo: ${saldo}")
-    else:
-        print("Fondos insuficientes o monto no válido.")
+def realizar_retiro(retiro,historial):
+    historial.append(f"Saldo: {retiro}")
+    saldo -= retiro
+    print(f"Retiro exitoso. Nuevo saldo: ${saldo}")
+    return saldo
+    
