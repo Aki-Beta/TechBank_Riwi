b633e19de03c5e429737327876db2bb62ef553fa
DanArci <DANIELARCINIEGASPUA@GMAIL.COM>
2026-03-04 15:08:01 -0500

    Correccion de buenas practicas y eliminacion de la llamada a la funcion

--- intentos.py
+++ intentos.py
@@ -4,29 +4,23 @@ def validacion():
  intentosfallidos = 0
  usuarioreal = "user"
  passwordreal = "1234"
-
+ 
  while len(intentos) <= 2:
-    
-    
-
-    usuario = input("introduce el usuario ")
-    password = input("introduce la contraseña ")
-
-    if usuario != usuarioreal:
-      intentos.append(usuario)
-      intentosfallidos = +1
   
-      if password != passwordreal:
-
-       intentoscontra.append(usuario)
-       print (" contraseña incorrecTA")
-
+  usuario = input("introduce el usuario ")
+  password = input("introduce la contraseña ")
+ 
+   if usuario != usuarioreal:
+    intentos.append(usuario)
+    intentosfallidos = +1
+  
+    if password != passwordreal:
+     intentoscontra.append(usuario)
+     print (" contraseña incorrecTA")
     elif usuario == usuarioreal:
-      intentos.append(usuario)
-      if password == passwordreal:
-        print("Correcto")
+     intentos.append(usuario)
+     if password == passwordreal:
+      print("Correcto")
+      
  if intentosfallidos == 3:
-  print(" usuario bloqueado") 
-validacion()    
-    
-
+  print(" usuario bloqueado")  
