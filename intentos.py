feb99c1c25d2be1883b9d2dacef10e0dfa3edb1b
franciscomartinezpenata-blip <franciscomartinezpenata@gmail.com>
2026-03-04 11:39:59 -0500

    Update intentos.py

--- intentos.py
+++ intentos.py
@@ -3,7 +3,7 @@ while intentos> 0:
     usuario = input("introduce el usuario ")
     password = input("introduce la contraseña ")
 
-    if usuario == "tyler" and password == "tyler123":
+    if usuario == "tyler" and password == "user1234":
         print("has entrado al sistema")
         break
     else:
@@ -11,3 +11,4 @@ while intentos> 0:
         print (f"incorrecto. te queda {intentos} intentos. \n")
     if intentos == 0:
         print ("cuenta bloqueada")
+
