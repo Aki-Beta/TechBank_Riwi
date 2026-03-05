4a06d8fdf91178d5fb38f0e0bacd5ba75cd728f0
julianvanegs <julivanegascri@gmail.com>
2026-03-05 12:16:44 -0500

    Refactor welcome function and add operation menu

--- configuracion.py
+++ configuracion.py
@@ -1,6 +1,35 @@
-def bienvenida():
-    print("Bienvenidos al Banco ")
-    
-    
-    
+from validacion import inicio
+from mostrarmenu import mostrarmenu
+from operaciones import consultar_saldo
+from operaciones import depositar
+from operaciones import retirar
+from operaciones import mostrar_historial
+from validacion_numerica import pedirNumero
+print("BIENVENIDO") 
 
+
+saldo = 1000 
+print("¿CUÁNTAS OPERACIONES DESEA REALIZAR?: ")
+operaciones = pedirNumero()
+
+
+historial = []
+
+for i in range(operaciones):
+    mostrarmenu()
+    print("DIGITE EL NUMERO DE LA OPERACION: ")
+    operacion = pedirNumero()
+    if operacion == "1":
+        saldo = consultar_saldo(saldo, historial)
+
+    elif operacion == "2":
+        saldo = depositar(saldo, historial)
+    
+    elif operacion == "3":
+        saldo = retirar(saldo, historial)
+
+    elif operacion =="4":
+        mostrar_historial(historial)
+    
+    else:
+        print("Opcion invalida")
