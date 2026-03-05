7a237d6396c64de700e0831eafbae1c63697c06f
josel <jrruidiaz811@gmail.com>
2026-03-05 14:38:04 -0500

    2 modificacion

--- autenticacion.py
+++ autenticacion.py
@@ -3,3 +3,4 @@ usuario = input("Ingrese su nombre de usuario: ")
 contraseña = input("Ingrese su contraseña: ")   
 
 if usuario == "carlos" and contraseña == "1234":
+    print("¡Bienvenido, Carlos!")
