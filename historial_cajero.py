71dd04342623e68974e439a4dba878b3df4f0199
angelarnache <adavicde@gmail.com>
2026-03-05 15:24:23 -0500

    historial_simple
    ultima correccion

--- historial_cajero.py
+++ historial_cajero.py
@@ -2,5 +2,6 @@ def mostrar_historial(historial):
     print("---historial de cajero---")
     if len(historial) == 0:
         print("historial vacio")
-    for movimientos in range(historial):
+    else:
+        for movimientos in range(historial):
         print(movimientos)
