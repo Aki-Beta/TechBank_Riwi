d7fb869d6496907868fe5577efba6f08a0881a87
Jose Rangel <josemrn2006@gmail.com>
2026-03-05 15:33:32 -0500

    Importación de ramas

--- menunuevo.py
+++ menunuevo.py
@@ -1,32 +1,11 @@
-def menu_principal():
-    saldo= 1000
-    saldo = get_saldo_actual()
-
-    # BIENVENIDA
-    print("🎉 === Bienvenido a TechBanck Riwi Digital ===")
-    input("Presiona Enter para continuar...")
-
-    # BUCLE PRINCIPAL
-    continuar = True
-    while continuar:
-        opcion = menu_principal(saldo)
-        
-        if opcion == 1:
-            # Función de compañero 1
-            pass  # print(f"Saldo actual: ${saldo:.2f}")
-            
-        elif opcion == 2:
-            # Función de compañero 2  
-            pass  # retirar_dinero(saldo)
-            
-        elif opcion == 3:
-            # Función de compañero 3
-            pass  # depositar_dinero(saldo)
-            
-        elif opcion == 4:
-            # Función de compañero 4
-            pass  # mostrar_historial()
-            
-        elif opcion == 5:
-            print("\n👋 ¡Gracias por usar TechBanck Riwi Digital!")
-            continuar = False
+from validacion_numeros import pedirNumero
+def menu_menuprincipal():
+    print("")
+    print("Elija una opción")
+    print("1. Consultar saldo")
+    print("2. Retirar dinero")
+    print("3. Depositar dinero")
+    print("4. Mostrar historial")
+    print("5. Salir")
+    opcion = pedirNumero()
+    return opcion
