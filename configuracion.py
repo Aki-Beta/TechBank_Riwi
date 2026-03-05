4c7f6a8981c7c40a51ff1b4659cbdd00c7f4ecf6
johann <joelhernava@gmail.com>
2026-03-05 15:46:50 -0500

    ultimo cambio

--- configuracion.py
+++ configuracion.py
@@ -1,29 +1,11 @@
+def configuracion_inicial():
+    print("===================================")
+    print("=     :BIENVENIDOS AL BANCO       =")
+    print("===================================")
+    input("PRESIONA ENTER PARA CONTINUAR    ")
+    print("")
+    saldo=1000
+    return saldo
 
-print("BIENVENIDO") 
-
-
-saldo = 1000 
-print("¿CUÁNTAS OPERACIONES DESEA REALIZAR?: ")
-operaciones = pedirNumero()
-
-
-historial = []
-
-for i in range(operaciones):
-    mostrarmenu()
-    print("DIGITE EL NUMERO DE LA OPERACION: ")
-    operacion = pedirNumero()
-    if operacion == "1":
-        saldo = consultar_saldo(saldo, historial)
-
-    elif operacion == "2":
-        saldo = depositar(saldo, historial)
     
-    elif operacion == "3":
-        saldo = retirar(saldo, historial)
 
-    elif operacion =="4":
-        mostrar_historial(historial)
-    
-    else:
-        print("Opcion invalida")
