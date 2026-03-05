ceb0938115c33d440d7dba2ecedf32a50a363f32
andres cassiani <andrescassiani46@gmail.com>
2026-03-05 15:38:12 -0500

    archivo 2 parte

--- deposito.py
+++ deposito.py
@@ -2,5 +2,4 @@ def realizar_deposito(deposito,historial,saldo):
     saldo*= deposito
     historial.append(f"saldo: {saldo}")
     print(" ")
-    print(f"Deposito realizado con exito, tu nuevo saldo es: {saldo}")
-    return saldo
+  
