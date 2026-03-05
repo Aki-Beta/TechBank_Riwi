e58b18ee3e53cd8c1088c846acf278d627c99f54
andres cassiani <andrescassiani46@gmail.com>
2026-03-05 15:37:23 -0500

    nuevo

--- deposito.py
+++ deposito.py
@@ -1,18 +1,6 @@
-def deposito(saldo, historial):
-    while True:
-        try:
-            valor = float(input("Ingrese el monto a depositar: "))
-            if valor <= 0:
-                print("El monto debe ser mayor a cero. Intente nuevamente.")
-            else:
-                saldo += valor
-                historial.append(f"Depósito: +${valor:.2f}")
-                print(f"Depósito exitoso. Nuevo saldo: ${saldo:.2f}")
-                return saldo, historial
-        except ValueError:
-            print("Entrada no válida. Por favor, ingrese un número.")
-
-saldo = 1000
-historial = []
-
-saldo, historial = deposito(saldo, historial)
+def realizar_deposito(deposito,historial,saldo):
+    saldo*= deposito
+    historial.append(f"saldo: {saldo}")
+    print(" ")
+    print(f"Deposito realizado con exito, tu nuevo saldo es: {saldo}")
+    return saldo
