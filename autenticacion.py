27ffdb22b9080924eac8a5568532960df0bc9d40
angelan garcia <angelagarciatorres1998@gmail.com>
2026-03-05 23:31:30 -0500

    merge autenticacion

--- autenticacion.py
+++ autenticacion.py
@@ -1,7 +1,10 @@
-usuario = input("Ingrese su nombre de usuario: ")
+def autenticacion():
+    usuario = input("Ingrese su nombre de usuario: ")
+    contraseña = input("Ingrese su contraseña: ")
 
-contraseña = input("Ingrese su contraseña: ")   
-
-if usuario == "carlos" and contraseña == "1234":
-    print("¡Bienvenido, Carlos!")
-else:    print("Usuario o contraseña incorrectos. Inténtalo de nuevo.")
+    if usuario == "carlos" and contraseña == "1234":
+        print("¡Bienvenido, Carlos!")
+        return True
+    else:
+        print("Usuario o contraseña incorrectos. Inténtalo de nuevo.")
+        return False
