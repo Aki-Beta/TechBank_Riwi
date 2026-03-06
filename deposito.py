49bc2618de95e9a7f5485a317615e33afae340cc
julianvanegs <julivanegascri@gmail.com>
2026-03-06 12:00:13 -0500

    Update deposit function to log deposit amount

--- deposito.py
+++ deposito.py
@@ -1,6 +1,6 @@
 def realizar_deposito(deposito,historial,saldo):
     saldo*= deposito
-    historial.append(f"saldo: {saldo}")
+    historial.append(f"Has depositado: {deposito} ")
     print(" ")
     print(f"Deposito realizado con exito, tu nuevo saldo es: {saldo}")
     return saldo
